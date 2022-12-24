2a8f77fb70877e41924c0054e9e9b9d24903e3f1
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-24 05:29:41 +0100

    commit zero

---
+++ Ninja Legends Script pastebin roblox Ninja Legends Gui .lua
@@ -0,0 +1,11 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:HttpGet("https://pastebin.com/raw/zgpJcGhg", true))()
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							The script for ninja legends is pretty easy to understand, it has auto buy items, auto sell items, a
+						
+]]
